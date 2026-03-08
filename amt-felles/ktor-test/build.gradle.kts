c627ac7dc4b6af0776737b61315e1f4c3662c2db
Roar Skinderviken <roar.skinderviken@gmail.com>
2026-03-08 16:45:02 +0100

    Optimalisert dependencies #2

--- amt-felles/ktor-test/build.gradle.kts
+++ amt-felles/ktor-test/build.gradle.kts
@@ -11,20 +11,7 @@ dependencies {
     api(project(":amt-lib:lib:testing"))
     implementation(project(":amt-felles:ktor"))
 
-    implementation(libs.kafka.clients)
-    implementation(libs.logback.classic)
-
     implementation(libs.testcontainers.postgresql)
-
-    implementation(libs.bundles.database)
-
-    implementation(libs.caffeine)
-
-    api(libs.bundles.kotest)
-
-    api(libs.junit.jupiter.params)
-    api(libs.kotlin.test.junit5)
-    api(libs.mockk)
 }
 
 kotlin {
