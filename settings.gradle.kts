6a513965cd5d03cfea82816484d2a5aae49e4db7
Roar Skinderviken <roar.skinderviken@gmail.com>
2026-03-09 16:50:35 +0100

    Flyttet modulene i amt-lib ett nivå opp

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,18 +4,10 @@ include(
     "amt-deltaker",
     "amt-deltaker-bff",
     "amt-distribusjon",
-    "amt-felles",
     "amt-felles:ktor",
     "amt-felles:ktor-test",
-    "amt-lib:lib",
-    "amt-lib:lib:kafka",
-    "amt-lib:lib:testing",
-    "amt-lib:lib:utils",
-    "amt-lib:lib:models",
+    "amt-lib:kafka",
+    "amt-lib:testing",
+    "amt-lib:utils",
+    "amt-lib:models",
 )
-
-// Renamer moduler i amt-lib til kortere navn for å unngå lange modulnavn i avhengigheter
-findProject(":amt-lib:lib:kafka")?.name = "kafka"
-findProject(":amt-lib:lib:testing")?.name = "testing"
-findProject(":amt-lib:lib:utils")?.name = "utils"
-findProject(":amt-lib:lib:models")?.name = "models"
