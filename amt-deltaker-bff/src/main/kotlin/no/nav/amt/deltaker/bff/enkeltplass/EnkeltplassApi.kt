7acb9cf0ad92dd610cd819b7f2c93e975c4da8bd
Izis Frøyhov <izis.froyhov@nav.no>
2026-03-30 14:57:23 +0200

    Lager enkeltplass api

---
+++ amt-deltaker-bff/src/main/kotlin/no/nav/amt/deltaker/bff/enkeltplass/EnkeltplassApi.kt
@@ -0,0 +1,34 @@
+package no.nav.amt.deltaker.bff.enkeltplass
+
+import io.ktor.server.auth.authenticate
+import io.ktor.server.routing.Routing
+import io.ktor.server.routing.post
+
+fun Routing.registerEnkeltplassApi() {
+    authenticate("VEILEDER") {
+        authenticate("VEILEDER") {
+            /*
+                Oppretter utkast for en enkeltplass deltaker.
+                Opprettes i handlingen "Del utkast"
+                Status: Kladd/utkast -> Utkast
+                @Return no.nav.amt.deltaker.bff.veileder.api.response.DeltakerResponse
+             */
+            post("/enkeltplass-utkast/{deltakerId}") {
+                // val request = call.receive<EnkeltplassUtkastRequest>()
+                throw NotImplementedError("Dette er ikke implementert.")
+            }
+
+            /*
+               Direktepåmelding av enkeltplass  deltaker uten at utkast/deltakelsen er delt med innbygger
+               Handling: "Meld på uten å dele utkast"
+               Status Kladd/Utkast -> Venter på oppstart/søkt inn
+               @Returns no.nav.amt.deltaker.bff.veileder.api.response.DeltakerResponse
+             */
+            post("/enkeltplass-utkast/{deltakerId}/meld-paa-direkte") {
+                // val request = call.receive<EnkeltplassUtkastRequest>()
+
+                throw NotImplementedError("Dette er ikke implementert.")
+            }
+        }
+    }
+}
