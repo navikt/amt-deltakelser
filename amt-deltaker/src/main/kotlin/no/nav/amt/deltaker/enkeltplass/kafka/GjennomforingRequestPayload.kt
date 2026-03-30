dab15101272b6ba48cc4d77bf636ebc3ecf10f37
Roar Skinderviken <roar.skinderviken@gmail.com>
2026-03-30 13:07:55 +0200

    Lagt til GjennomforingRequestProducer og payload

---
+++ amt-deltaker/src/main/kotlin/no/nav/amt/deltaker/enkeltplass/kafka/GjennomforingRequestPayload.kt
@@ -0,0 +1,17 @@
+package no.nav.amt.deltaker.enkeltplass.kafka
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode
+import java.util.UUID
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.SIMPLE_NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
+sealed interface GjennomforingRequestPayload {
+    val gjennomforingId: UUID
+
+    data class OpprettGjennomforing(
+        override val gjennomforingId: UUID,
+        val tiltakskode: Tiltakskode,
+        val prisinformasjon: String,
+        val organisasjonsnummer: String,
+    ) : GjennomforingRequestPayload
+}
---
+++ amt-deltaker/src/main/kotlin/no/nav/amt/deltaker/enkeltplass/kafka/GjennomforingRequestProducer.kt
@@ -0,0 +1,16 @@
+package no.nav.amt.deltaker.enkeltplass.kafka
+
+import no.nav.amt.deltaker.Environment
+import no.nav.amt.lib.outbox.OutboxService
+
+class GjennomforingRequestProducer(
+    private val outboxService: OutboxService,
+) {
+    fun produce(payload: GjennomforingRequestPayload) {
+        outboxService.insertRecord(
+            topic = Environment.GJENNOMFORING_REQUEST_TOPIC,
+            key = payload.gjennomforingId,
+            value = payload,
+        )
+    }
+}
