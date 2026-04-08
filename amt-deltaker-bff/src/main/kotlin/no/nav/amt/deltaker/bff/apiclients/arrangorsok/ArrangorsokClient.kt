516ff1a651db4909e8068f6d0258a3c689be2546
Roar Skinderviken <7499642+roar-skinderviken@users.noreply.github.com>
2026-04-08 12:26:30 +0200

    Lagt til URL-enkoding i arrangørsøk (#97)

--- amt-deltaker-bff/src/main/kotlin/no/nav/amt/deltaker/bff/apiclients/arrangorsok/ArrangorsokClient.kt
+++ amt-deltaker-bff/src/main/kotlin/no/nav/amt/deltaker/bff/apiclients/arrangorsok/ArrangorsokClient.kt
@@ -2,6 +2,7 @@ package no.nav.amt.deltaker.bff.apiclients.arrangorsok
 
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.http.encodeURLPath
 import no.nav.amt.lib.ktor.auth.AzureAdTokenClient
 import no.nav.amt.lib.ktor.clients.ApiClientBase
 import no.nav.amt.lib.ktor.clients.failIfNotSuccess
@@ -17,7 +18,7 @@ class ArrangorsokClient(
         httpClient = httpClient,
         azureAdTokenClient = azureAdTokenClient,
     ) {
-    suspend fun hovedenhetSok(term: String): List<EnhetResponse> = performGet("api/v1/arrangor/hovedenhet/sok/$term")
+    suspend fun hovedenhetSok(term: String): List<EnhetResponse> = performGet("api/v1/arrangor/hovedenhet/sok/${term.encodeURLPath()}")
         .failIfNotSuccess("Kunne ikke hente hovedenheter fra Mulighetsrommet")
         .body()
 
