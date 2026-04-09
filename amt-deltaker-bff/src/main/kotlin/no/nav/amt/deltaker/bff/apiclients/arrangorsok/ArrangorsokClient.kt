fed6572f608f602d47792e80bc0f690a3b2dc6ea
Roar Skinderviken <7499642+roar-skinderviken@users.noreply.github.com>
2026-04-09 18:01:07 +0200

    Endret ArrangorsokClient og ArrangorsokApi til å søke kun i underenheter (#103)

--- amt-deltaker-bff/src/main/kotlin/no/nav/amt/deltaker/bff/apiclients/arrangorsok/ArrangorsokClient.kt
+++ amt-deltaker-bff/src/main/kotlin/no/nav/amt/deltaker/bff/apiclients/arrangorsok/ArrangorsokClient.kt
@@ -2,7 +2,7 @@ package no.nav.amt.deltaker.bff.apiclients.arrangorsok
 
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
-import io.ktor.http.encodeURLPath
+import io.ktor.client.request.parameter
 import no.nav.amt.lib.ktor.auth.AzureAdTokenClient
 import no.nav.amt.lib.ktor.clients.ApiClientBase
 import no.nav.amt.lib.ktor.clients.failIfNotSuccess
@@ -18,11 +18,7 @@ class ArrangorsokClient(
         httpClient = httpClient,
         azureAdTokenClient = azureAdTokenClient,
     ) {
-    suspend fun hovedenhetSok(term: String): List<EnhetResponse> = performGet("api/v1/arrangor/hovedenhet/sok/${term.encodeURLPath()}")
-        .failIfNotSuccess("Kunne ikke hente hovedenheter fra Mulighetsrommet")
-        .body()
-
-    suspend fun hentUnderenheter(orgnummer: String): List<EnhetResponse> = performGet("api/v1/arrangor/hovedenhet/$orgnummer/underenheter")
-        .failIfNotSuccess("Kunne ikke hente underenheter fra Mulighetsrommet for orgnummer $orgnummer")
-        .body()
+    suspend fun underenhetSok(term: String): List<EnhetResponse> = performGet("api/v1/arrangor/underenhet") {
+        parameter("query", term)
+    }.failIfNotSuccess("Kunne ikke hente underenheter fra Mulighetsrommet").body()
 }
