589686cb7b08cf378fb8fb1f2376b0575ac6c8b3
Roar Skinderviken <roar.skinderviken@gmail.com>
2026-04-13 23:02:12 +0200

    Fjernet overflødig dep

--- amt-felles/intern-api-kontrakter/build.gradle.kts
+++ amt-felles/intern-api-kontrakter/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
 }
 
 dependencies {
-    implementation(project(":amt-felles:ktor"))
     implementation(project(":amt-lib:models"))
     implementation(project(":amt-lib:utils"))
     testImplementation(project(":amt-lib:testing"))
