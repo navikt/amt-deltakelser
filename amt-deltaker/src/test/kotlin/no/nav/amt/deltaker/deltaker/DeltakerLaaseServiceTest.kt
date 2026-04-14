56b4bb7ef91cb90d064cd725ae9d0e94b343bee9
Roar Skinderviken <7499642+roar-skinderviken@users.noreply.github.com>
2026-04-14 15:55:08 +0200

    Endret låselogikk for tilfeller der alle deltakelser er låst (#126)

--- amt-deltaker/src/test/kotlin/no/nav/amt/deltaker/deltaker/DeltakerLaaseServiceTest.kt
+++ amt-deltaker/src/test/kotlin/no/nav/amt/deltaker/deltaker/DeltakerLaaseServiceTest.kt
@@ -1,6 +1,7 @@
 package no.nav.amt.deltaker.deltaker
 
 import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.shouldBe
 import io.mockk.clearAllMocks
 import io.mockk.every
@@ -23,10 +24,13 @@ class DeltakerLaaseServiceTest {
     private val mockImportertFraArenaRepository = mockk<ImportertFraArenaRepository>()
     private val sut = DeltakerLaaseService(mockDeltakerRepository, mockImportertFraArenaRepository)
 
-    private val deltakerInTest = lagDeltaker()
+    private val deltakerInTest = lagDeltaker(
+        status = lagDeltakerStatus(DeltakerStatus.Type.DELTAR),
+    )
     private val tidligereDeltakerInTest = lagDeltaker(
-        deltakerliste = deltakerInTest.deltakerliste,
+        status = lagDeltakerStatus(DeltakerStatus.Type.VENTER_PA_OPPSTART),
         navBruker = deltakerInTest.navBruker,
+        deltakerliste = deltakerInTest.deltakerliste,
     )
 
     private val importertFraArena = lagImportertFraArena(
@@ -67,7 +71,7 @@ class DeltakerLaaseServiceTest {
         }
 
         @Test
-        fun `skal returrnere false hvis deltaker ikke har tidligere deltakelser`() {
+        fun `skal returnere false hvis deltaker ikke har tidligere deltakelser`() {
             // Arrange
             every {
                 mockDeltakerRepository.getFlereForPerson(
@@ -84,7 +88,7 @@ class DeltakerLaaseServiceTest {
         }
 
         @Test
-        fun `skal returrnere true hvis deltaker ikke er nyeste deltaker`() {
+        fun `skal returnere true hvis deltaker ikke er nyeste deltaker`() {
             // Arrange
             every {
                 mockDeltakerRepository.getFlereForPerson(
@@ -98,6 +102,26 @@ class DeltakerLaaseServiceTest {
 
             // Assert
             result shouldBe true
+        }
+
+        @Test
+        fun `skal returnere false hvis begge deltakelser er inaktive`() {
+            // Arrange
+            every {
+                mockDeltakerRepository.getFlereForPerson(
+                    personIdent = deltakerInTest.navBruker.personident,
+                    deltakerlisteId = deltakerInTest.deltakerliste.id,
+                )
+            } returns listOf(
+                deltakerInTest.copy(status = lagDeltakerStatus(DeltakerStatus.Type.HAR_SLUTTET)),
+                tidligereDeltakerInTest.copy(
+                    status = lagDeltakerStatus(DeltakerStatus.Type.IKKE_AKTUELL),
+                ),
+            )
+
+            // Act & Assert
+            sut.erLaastForEndringer(deltakerInTest).shouldBeFalse()
+            sut.erLaastForEndringer(tidligereDeltakerInTest).shouldBeFalse()
         }
 
         @Test
