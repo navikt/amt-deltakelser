9ea5290194746598de4f0622a7cd9dc8ef5c9777
Roar Skinderviken <roar.skinderviken@gmail.com>
2026-04-20 14:53:14 +0200

    Fjernet gjennomføring-id fra blacklist

--- amt-lib/models/src/main/kotlin/no/nav/amt/lib/models/deltakerliste/kafka/GjennomforingV2KafkaPayload.kt
+++ amt-lib/models/src/main/kotlin/no/nav/amt/lib/models/deltakerliste/kafka/GjennomforingV2KafkaPayload.kt
@@ -128,9 +128,7 @@ sealed interface GjennomforingV2KafkaPayload {
 
         // dette er gjenomføringene som har blitt opprettet med feil påmeldingstype,
         // og som derfor må ignoreres av consumere
-        val gjennomforingBlacklist = setOf(
-            UUID.fromString("216cf6e3-6259-4310-b0c2-9188dd07bb95"),
-        )
+        val gjennomforingBlacklist = emptySet<UUID>()
 
         val direktetiltak =
             setOf(
