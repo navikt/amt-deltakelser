ce7e9e65aef95b8a014c2dfb3152271c43e80394
Izis Frøyhov <izis.froyhov@nav.no>
2026-04-30 11:31:14 +0200

    Endrer pakkestruktur

--- amt-deltaker/src/main/kotlin/no/nav/amt/deltaker/api/response/ResponseMapper.kt
+++ amt-deltaker/src/main/kotlin/no/nav/amt/deltaker/api/response/ResponseMapper.kt
@@ -6,56 +6,54 @@ import no.nav.amt.internapi.paamelding.response.OpprettKladdResponse
 import no.nav.amt.internapi.paamelding.response.UtkastResponse
 import no.nav.amt.lib.models.deltaker.DeltakerHistorikk
 
-class ResponseMapper {
-    companion object {
-        fun utkastResponseFromDeltaker(
-            deltaker: Deltaker,
-            historikk: List<DeltakerHistorikk>,
-        ) = with(deltaker) {
-            UtkastResponse(
-                id = id,
-                startdato = startdato,
-                sluttdato = sluttdato,
-                dagerPerUke = dagerPerUke,
-                deltakelsesprosent = deltakelsesprosent,
-                bakgrunnsinformasjon = bakgrunnsinformasjon,
-                deltakelsesinnhold = deltakelsesinnhold,
-                status = status,
-                historikk = historikk,
-                erManueltDeltMedArrangor = erManueltDeltMedArrangor,
-            )
-        }
+object ResponseMapper {
+    fun utkastResponseFromDeltaker(
+        deltaker: Deltaker,
+        historikk: List<DeltakerHistorikk>,
+    ) = with(deltaker) {
+        UtkastResponse(
+            id = id,
+            startdato = startdato,
+            sluttdato = sluttdato,
+            dagerPerUke = dagerPerUke,
+            deltakelsesprosent = deltakelsesprosent,
+            bakgrunnsinformasjon = bakgrunnsinformasjon,
+            deltakelsesinnhold = deltakelsesinnhold,
+            status = status,
+            historikk = historikk,
+            erManueltDeltMedArrangor = erManueltDeltMedArrangor,
+        )
+    }
 
-        fun opprettKladdResponseFromDeltaker(deltaker: Deltaker) = with(deltaker) {
-            OpprettKladdResponse(
-                id = id,
-                navBruker = navBruker,
-                deltakerlisteId = deltakerliste.id,
-                startdato = startdato,
-                sluttdato = sluttdato,
-                dagerPerUke = dagerPerUke,
-                deltakelsesprosent = deltakelsesprosent,
-                bakgrunnsinformasjon = bakgrunnsinformasjon,
-                deltakelsesinnhold = deltakelsesinnhold!!,
-                status = status,
-            )
-        }
+    fun opprettKladdResponseFromDeltaker(deltaker: Deltaker) = with(deltaker) {
+        OpprettKladdResponse(
+            id = id,
+            navBruker = navBruker,
+            deltakerlisteId = deltakerliste.id,
+            startdato = startdato,
+            sluttdato = sluttdato,
+            dagerPerUke = dagerPerUke,
+            deltakelsesprosent = deltakelsesprosent,
+            bakgrunnsinformasjon = bakgrunnsinformasjon,
+            deltakelsesinnhold = deltakelsesinnhold!!,
+            status = status,
+        )
+    }
 
-        fun deltakerEndringResponseFromDeltaker(
-            deltaker: Deltaker,
-            historikk: List<DeltakerHistorikk>,
-        ) = with(deltaker) {
-            DeltakerEndringResponse(
-                id = id,
-                startdato = startdato,
-                sluttdato = sluttdato,
-                dagerPerUke = dagerPerUke,
-                deltakelsesprosent = deltakelsesprosent,
-                bakgrunnsinformasjon = bakgrunnsinformasjon,
-                deltakelsesinnhold = deltakelsesinnhold,
-                status = status,
-                historikk = historikk,
-            )
-        }
+    fun deltakerEndringResponseFromDeltaker(
+        deltaker: Deltaker,
+        historikk: List<DeltakerHistorikk>,
+    ) = with(deltaker) {
+        DeltakerEndringResponse(
+            id = id,
+            startdato = startdato,
+            sluttdato = sluttdato,
+            dagerPerUke = dagerPerUke,
+            deltakelsesprosent = deltakelsesprosent,
+            bakgrunnsinformasjon = bakgrunnsinformasjon,
+            deltakelsesinnhold = deltakelsesinnhold,
+            status = status,
+            historikk = historikk,
+        )
     }
 }
