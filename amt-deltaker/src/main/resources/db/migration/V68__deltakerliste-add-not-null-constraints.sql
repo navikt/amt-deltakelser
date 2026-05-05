6bd98ce63b9a3c299028ab6e40b83866abb09de9
Roar Skinderviken <roar.skinderviken@gmail.com>
2026-05-05 09:40:39 +0200

    Fjernet ALTER COLUMN arrangor_id SET NOT NULL

--- amt-deltaker/src/main/resources/db/migration/V68__deltakerliste-add-not-null-constraints.sql
+++ amt-deltaker/src/main/resources/db/migration/V68__deltakerliste-add-not-null-constraints.sql
@@ -1,5 +1,4 @@
 ALTER TABLE deltakerliste
-    ALTER COLUMN arrangor_id SET NOT NULL,
     ALTER COLUMN status  SET NOT NULL,
     ALTER COLUMN pameldingstype  SET NOT NULL,
     ALTER COLUMN oppstart SET NOT NULL;
