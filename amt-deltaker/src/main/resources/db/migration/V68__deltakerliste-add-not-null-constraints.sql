03f30e45510fb431e910437d5033b843d2d319ea
Roar Skinderviken <roar.skinderviken@gmail.com>
2026-05-05 08:18:26 +0200

    Lagt til NOT NULL constraints for deltakerliste

---
+++ amt-deltaker/src/main/resources/db/migration/V68__deltakerliste-add-not-null-constraints.sql
@@ -0,0 +1,5 @@
+ALTER TABLE deltakerliste
+    ALTER COLUMN arrangor_id SET NOT NULL,
+    ALTER COLUMN status  SET NOT NULL,
+    ALTER COLUMN pameldingstype  SET NOT NULL,
+    ALTER COLUMN oppstart SET NOT NULL;
