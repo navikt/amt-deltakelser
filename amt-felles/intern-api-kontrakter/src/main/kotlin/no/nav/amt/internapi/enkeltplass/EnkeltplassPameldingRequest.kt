36b2d984c7ff77f9ea46abd13bce66e8b6849c49
Roar Skinderviken <roar.skinderviken@gmail.com>
2026-05-18 15:15:40 +0200

    Enkeltplass lagre utkast

--- amt-felles/intern-api-kontrakter/src/main/kotlin/no/nav/amt/internapi/enkeltplass/EnkeltplassPameldingRequest.kt
+++ amt-felles/intern-api-kontrakter/src/main/kotlin/no/nav/amt/internapi/enkeltplass/EnkeltplassPameldingRequest.kt
@@ -1,13 +1,18 @@
 package no.nav.amt.internapi.enkeltplass
 
+import no.nav.amt.lib.models.deltakerliste.SertifiseringValg
 import java.time.LocalDate
+import java.util.UUID
 
+// TODO: Denne klassen er veldig lik OppdaterEnkeltplassKladdRequest
 data class EnkeltplassPameldingRequest(
     val beskrivelse: String,
     val prisinformasjon: String,
     val arrangorUnderenhet: String,
     val startdato: LocalDate? = null,
     val sluttdato: LocalDate? = null,
+    val kodeverkValg: Set<UUID>? = null,
+    val sertifiseringValg: Set<SertifiseringValg>? = null,
 ) {
     fun sanitized() = copy(
         beskrivelse = beskrivelse.sanitizeBeskrivelse(),
