d56f57703511bcfe1e8910bd93e54a469fc9e2c1
robertkittilsen <robert.graarup.kittilsen@nav.no>
2026-05-26 12:34:07 +0200

    returnerer 400 bad request ved valideringfeil av gyldigFra

--- amt-deltaker/src/main/kotlin/no/nav/amt/deltaker/veileder/endring/extensions/EndreDeltakelsesmengdeExtensions.kt
+++ amt-deltaker/src/main/kotlin/no/nav/amt/deltaker/veileder/endring/extensions/EndreDeltakelsesmengdeExtensions.kt
@@ -13,7 +13,7 @@ fun DeltakerEndring.Endring.EndreDeltakelsesmengde.hasChanges(deltakelsemengder:
     return deltakelsemengder.validerNyDeltakelsesmengde(nyDeltakelsesmengde)
 }
 
-fun DeltakerEndring.Endring.EndreDeltakelsesmengde.endreDeltakelsesmengde(deltaker: Deltaker): VellykketEndring {
+fun DeltakerEndring.Endring.EndreDeltakelsesmengde.validerGyldigFra(deltaker: Deltaker) {
     val nyDeltakelsesmengde = this.toDeltakelsesmengde(LocalDateTime.now())
 
     val startdato = deltaker.startdato
@@ -25,6 +25,13 @@ fun DeltakerEndring.Endring.EndreDeltakelsesmengde.endreDeltakelsesmengde(deltak
     require(sluttdato == null || nyDeltakelsesmengde.gyldigFra <= sluttdato) {
         "gyldigFra (${nyDeltakelsesmengde.gyldigFra}) kan ikke være etter sluttdato ($sluttdato)"
     }
+}
+
+fun DeltakerEndring.Endring.EndreDeltakelsesmengde.endreDeltakelsesmengde(deltaker: Deltaker): VellykketEndring {
+    val nyDeltakelsesmengde = this.toDeltakelsesmengde(LocalDateTime.now())
+
+    // Defence-in-depth: også validert i DeltakerService før runCatching for å gi 400 Bad Request
+    validerGyldigFra(deltaker)
 
     return if (nyDeltakelsesmengde.gyldigFra <= LocalDate.now()) {
         VellykketEndring(
