5f86743b717dc01799f37a5600faf0cb16108ba3
Jarle Fagerheim <jarle.fagerheim@nav.no>
2026-05-28 10:51:40 +0200

    lokal profil for amt-tiltaksarrangor-bff

--- amt-tiltaksarrangor-bff/src/main/kotlin/no/nav/tiltaksarrangor/melding/KafkaProducerConfig.kt
+++ amt-tiltaksarrangor-bff/src/main/kotlin/no/nav/tiltaksarrangor/melding/KafkaProducerConfig.kt
@@ -3,6 +3,7 @@ package no.nav.tiltaksarrangor.melding
 import no.nav.amt.lib.kafka.Producer
 import no.nav.amt.lib.kafka.config.KafkaConfig
 import no.nav.amt.lib.kafka.config.KafkaConfigImpl
+import no.nav.amt.lib.kafka.config.LocalKafkaConfig
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
@@ -12,6 +13,10 @@ class KafkaProducerConfig {
     @Bean
     @Profile("default")
     fun config(): KafkaConfig = KafkaConfigImpl()
+
+    @Bean
+    @Profile("local")
+    fun localConfig(): KafkaConfig = LocalKafkaConfig(System.getenv("KAFKA_BROKERS") ?: "localhost:9092")
 
     @Bean(destroyMethod = "close")
     fun producer(kafkaConfig: KafkaConfig) = Producer<String, String>(kafkaConfig)
