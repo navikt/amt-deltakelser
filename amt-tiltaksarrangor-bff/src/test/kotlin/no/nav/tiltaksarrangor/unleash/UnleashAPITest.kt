330a4539f76f41caf1d346171bc11507e75a6b5a
Roar Skinderviken <roar.skinderviken@gmail.com>
2026-06-14 16:05:36 +0200

    Migrert VeilederApiTest

--- amt-tiltaksarrangor-bff/src/test/kotlin/no/nav/tiltaksarrangor/unleash/UnleashAPITest.kt
+++ amt-tiltaksarrangor-bff/src/test/kotlin/no/nav/tiltaksarrangor/unleash/UnleashAPITest.kt
@@ -3,33 +3,36 @@ package no.nav.tiltaksarrangor.unleash
 import io.kotest.matchers.shouldBe
 import no.nav.tiltaksarrangor.IntegrationTest
 import org.junit.jupiter.api.Test
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc
 import org.springframework.http.HttpHeaders
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
 
-class UnleashAPITest : IntegrationTest() {
+@AutoConfigureMockMvc
+class UnleashAPITest(
+    private val mockMvc: MockMvc,
+) : IntegrationTest() {
     @Test
     fun `getFeaturetoggles - ikke autentisert - returnerer 401`() {
-        val response = sendRequest(
-            method = "GET",
-            path = "/unleash/api/feature?feature=amt-tiltaksarrangor-flate.driftsmelding&amt-tiltaksarrangor-flate.eksponer-kurs",
-        )
-
-        response.code shouldBe 401
+        mockMvc
+            .get("/unleash/api/feature?feature=amt-tiltaksarrangor-flate.driftsmelding&amt-tiltaksarrangor-flate.eksponer-kurs")
+            .andExpect { status { isUnauthorized() } }
     }
 
     @Test
     fun `getFeaturetoggles - autentisert - returnerer toggles`() {
-        val response = sendRequest(
-            method = "GET",
-            path = "/unleash/api/feature?feature=amt-tiltaksarrangor-flate.driftsmelding" +
-                "&feature=amt-tiltaksarrangor-flate.eksponer-kurs",
-            headers = mapOf(HttpHeaders.AUTHORIZATION to "Bearer ${getTokenxToken(fnr = "12345678910")}"),
-        )
+        val response = mockMvc
+            .get("/unleash/api/feature?feature=amt-tiltaksarrangor-flate.driftsmelding&feature=amt-tiltaksarrangor-flate.eksponer-kurs") {
+                header(HttpHeaders.AUTHORIZATION, "Bearer ${getTokenxToken(fnr = "12345678910")}")
+            }.andExpect { status { isOk() } }
+            .andReturn()
+            .response
+            .contentAsString
 
         val expectedJson =
             """
             {"amt-tiltaksarrangor-flate.driftsmelding":true,"amt-tiltaksarrangor-flate.eksponer-kurs":true}
             """.trimIndent()
-        response.code shouldBe 200
-        response.body.string() shouldBe expectedJson
+        response shouldBe expectedJson
     }
 }
