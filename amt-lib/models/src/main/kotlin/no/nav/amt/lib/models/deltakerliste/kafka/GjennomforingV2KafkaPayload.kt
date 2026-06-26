3d0018783b37d86bfd73e876ded00bf8fa662857
Izis Frøyhov <izis.froyhov@nav.no>
2026-06-26 14:54:33 +0200

    Midlertidig fjerne krav til påmeldingstype for at consumeren skal komme seg forbi gjennomføringer med feil påmeldingstype i prod

--- amt-lib/models/src/main/kotlin/no/nav/amt/lib/models/deltakerliste/kafka/GjennomforingV2KafkaPayload.kt
+++ amt-lib/models/src/main/kotlin/no/nav/amt/lib/models/deltakerliste/kafka/GjennomforingV2KafkaPayload.kt
@@ -44,10 +44,11 @@ sealed interface GjennomforingV2KafkaPayload {
     ) {
         if (antallDeltakere == 0) return
 
+        /*
         require(pameldingType == eksisterendePameldingstype || eksisterendePameldingstype == null) {
             "Påmeldingstype kan ikke endres for deltakerliste $id med deltakere"
         }
-
+*/
         require(oppstart == eksisterendeOppstartstype || eksisterendeOppstartstype == null) {
             "Oppstartstype kan ikke endres for deltakerliste $id med deltakere"
         }
