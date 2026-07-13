be73bb799de7006910dddcd5fbd27277b9374090
Jarle Fagerheim <jarle.fagerheim@nav.no>
2026-07-13 11:48:01 +0200

    rydding

--- amt-deltaker-bff/src/main/kotlin/no/nav/amt/deltaker/bff/navtiltakskoordinator/api/UlestHendelseApi.kt
+++ amt-deltaker-bff/src/main/kotlin/no/nav/amt/deltaker/bff/navtiltakskoordinator/api/UlestHendelseApi.kt
@@ -12,8 +12,8 @@ import java.util.UUID
 fun Routing.registerUlestHendelseApi(tiltakskoordinatorClient: TiltakskoordinatorClient) {
     authenticate(AuthLevel.TILTAKSKOORDINATOR.name) {
         delete("/tiltakskoordinator/ulest-hendelse/{id}") {
-val id = call.parameters["id"]?.let(UUID::fromString)
-    ?: throw IllegalArgumentException("Påkrevd URL parameter 'id' mangler.")
+            val id = call.parameters["id"]?.let(UUID::fromString)
+                ?: throw IllegalArgumentException("Påkrevd URL parameter 'id' mangler.")
             tiltakskoordinatorClient.slettUlestHendelse(id)
             call.respond(HttpStatusCode.NoContent)
         }
