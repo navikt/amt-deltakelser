53c4289f5db48cc72ce17688850ef845baa7120f
Jarle Fagerheim <jarle.fagerheim@nav.no>
2026-07-21 15:26:54 +0200

    rydding

--- amt-distribusjon/src/main/kotlin/no/nav/amt/distribusjon/journalforing/pdf/PdfDtoMappingUtils.kt
+++ amt-distribusjon/src/main/kotlin/no/nav/amt/distribusjon/journalforing/pdf/PdfDtoMappingUtils.kt
@@ -80,17 +80,6 @@ fun HendelseDeltaker.Deltakerliste.visningsnavn() = lagVisningsnavn(
     arrangorNavn = arrangor.visningsnavn(),
     opplaringKategoriseringValg = opplaringKategoriseringValg,
 )
-/*
-fun HendelseDeltaker.Deltakerliste.ingressVisningsnavn(): String = if (this.tiltak.tiltakskode.erOpplaeringstiltak()) {
-    "${this.navn} hos ${arrangor.visningsnavn()}"
-} else if (this.tiltak.tiltakskode == Tiltakskode.TILRETTELAGT_ARBEID_ORDINAER) {
-    // TAO skal ha forskjellig navn i tittel og ingress
-    "${tiltak.navn} hos ${arrangor.visningsnavn()}"
-} else {
-    tittelVisningsnavn()
-}
-
- */
 
 fun HendelseDeltaker.Deltakerliste.Arrangor.visningsnavn(): String = with(overordnetArrangor) {
     val visningsnavn = if (this == null || this.navn == "Ukjent Virksomhet") {
