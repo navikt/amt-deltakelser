f55e10551336212839d7b196a965ec259d08aedb
Roar Skinderviken <roar.skinderviken@gmail.com>
2026-07-24 16:51:11 +0200

    KtLint-fixes

--- amt-deltaker-bff/src/main/kotlin/no/nav/amt/deltaker/bff/veileder/api/response/DeltakerHistorikkResponse.kt
+++ amt-deltaker-bff/src/main/kotlin/no/nav/amt/deltaker/bff/veileder/api/response/DeltakerHistorikkResponse.kt
@@ -138,8 +138,12 @@ data class EnkeltplassOkonomiGodkjentResponse(
         enheter: Map<UUID, NavEnhet>,
         ansatte: Map<UUID, NavAnsatt>,
     ) : this(
-        endretAv = requireNotNull(ansatte[model.sistEndretAvNavAnsattId]?.navn) { "Fant ikke navn for Nav-ansatt med id=${model.sistEndretAvNavAnsattId}" },
-        endretAvEnhet = requireNotNull(enheter[model.sistEndretAvNavEnhetId]?.navn) { "Fant ikke navn for enhet med id=${model.sistEndretAvNavEnhetId}" },
+        endretAv = requireNotNull(ansatte[model.sistEndretAvNavAnsattId]?.navn) {
+            "Fant ikke navn for Nav-ansatt med id=${model.sistEndretAvNavAnsattId}"
+        },
+        endretAvEnhet = requireNotNull(enheter[model.sistEndretAvNavEnhetId]?.navn) {
+            "Fant ikke navn for enhet med id=${model.sistEndretAvNavEnhetId}"
+        },
         endret = model.sistEndret,
     )
 }
