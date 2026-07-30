e40b0d7f17eb3cd5b892debbbc6283882aa4f23f
Roar Skinderviken <roar.skinderviken@gmail.com>
2026-07-31 00:05:57 +0200

    JWT-fix

--- amt-tiltaksarrangor-bff/src/main/kotlin/no/nav/tiltaksarrangor/config/SecurityConfig.kt
+++ amt-tiltaksarrangor-bff/src/main/kotlin/no/nav/tiltaksarrangor/config/SecurityConfig.kt
@@ -1,5 +1,6 @@
 package no.nav.tiltaksarrangor.config
 
+import com.nimbusds.jose.jwk.JWK
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -10,6 +11,9 @@ import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClient
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder
 import org.springframework.security.oauth2.client.TokenExchangeOAuth2AuthorizedClientProvider
+import org.springframework.security.oauth2.client.endpoint.NimbusJwtClientAuthenticationParametersConverter
+import org.springframework.security.oauth2.client.endpoint.RestClientTokenExchangeTokenResponseClient
+import org.springframework.security.oauth2.client.endpoint.TokenExchangeGrantRequest
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
 import org.springframework.security.oauth2.client.web.client.support.OAuth2RestClientHttpServiceGroupConfigurer
 import org.springframework.security.web.SecurityFilterChain
@@ -33,18 +37,28 @@ class SecurityConfig {
         }.build()
 
     @Bean
-    fun authorizedClientManager(clientRegistrationRepository: ClientRegistrationRepository): OAuth2AuthorizedClientManager =
-        AuthorizedClientServiceOAuth2AuthorizedClientManager(
+    fun authorizedClientManager(clientRegistrationRepository: ClientRegistrationRepository): OAuth2AuthorizedClientManager {
+        val jwtConverter = NimbusJwtClientAuthenticationParametersConverter<TokenExchangeGrantRequest> { registration ->
+            JWK.parse(registration.clientSecret)
+        }
+        val tokenExchangeClient = RestClientTokenExchangeTokenResponseClient()
+        tokenExchangeClient.addParametersConverter { grantRequest -> jwtConverter.convert(grantRequest)!! }
+
+        val tokenExchangeProvider = TokenExchangeOAuth2AuthorizedClientProvider()
+        tokenExchangeProvider.setAccessTokenResponseClient(tokenExchangeClient)
+
+        return AuthorizedClientServiceOAuth2AuthorizedClientManager(
             clientRegistrationRepository,
             InMemoryOAuth2AuthorizedClientService(clientRegistrationRepository),
         ).apply {
             setAuthorizedClientProvider(
                 DelegatingOAuth2AuthorizedClientProvider(
                     OAuth2AuthorizedClientProviderBuilder.builder().clientCredentials().build(),
-                    TokenExchangeOAuth2AuthorizedClientProvider(),
+                    tokenExchangeProvider,
                 ),
             )
         }
+    }
 
     @Bean
     fun oauth2Configurer(manager: OAuth2AuthorizedClientManager): OAuth2RestClientHttpServiceGroupConfigurer =
