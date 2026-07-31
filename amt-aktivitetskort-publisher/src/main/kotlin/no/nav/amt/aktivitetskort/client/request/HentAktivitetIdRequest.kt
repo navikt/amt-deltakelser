8983194646c8aeee6bb5ccd0f4ac01b5795183e9
Jarle Fagerheim <jarle.fagerheim@nav.no>
2026-07-31 09:42:49 +0200

    Align tree exactly with commit 173989e

--- amt-aktivitetskort-publisher/src/main/kotlin/no/nav/amt/aktivitetskort/client/request/HentAktivitetIdRequest.kt
+++
@@ -1,6 +0,0 @@
-package no.nav.amt.aktivitetskort.client.request
-
-data class HentAktivitetIdRequest(
-    val arenaId: Long,
-    val aktivitetKategori: String = "TILTAKSAKTIVITET",
-)
--- amt-aktivitetskort-publisher/src/main/kotlin/no/nav/amt/aktivitetskort/client/response/HentArenaIdV2Response.kt
+++
@@ -1,6 +0,0 @@
-package no.nav.amt.aktivitetskort.client.response
-
-data class HentArenaIdV2Response(
-    val arenaId: String?,
-    val arenaHistId: String?,
-)
