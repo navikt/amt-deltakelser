7f10752f5297a4c10e32de7839dd54423df740a6
Roar Skinderviken <roar.skinderviken@gmail.com>
2026-08-22 12:00:16 +0200

    Lagt til tester for ValgteFelt

--- amt-lib/models/src/test/kotlin/no/nav/amt/lib/models/deltaker/OpplaringKategoriseringValgTest.kt
+++ amt-lib/models/src/test/kotlin/no/nav/amt/lib/models/deltaker/OpplaringKategoriseringValgTest.kt
@@ -9,6 +9,44 @@ import java.util.UUID
 
 class OpplaringKategoriseringValgTest {
     @Nested
+    inner class ValgteFeltTests {
+        @Test
+        fun `konstruktør i ValgteFelt skal kaste feil hvis type er sertifiseringer`() {
+            val thrown = shouldThrow<IllegalArgumentException> {
+                OpplaringKategoriseringValg.ValgteFelt(
+                    representerer = OpplaringKategoriseringType.SERTIFISERINGER,
+                    valg = mapOf(UUID.randomUUID() to "Sertifisering"),
+                )
+            }
+
+            thrown.message shouldBe "Sertifiseringer kan ikke representeres av ValgteFelt"
+        }
+
+        @Test
+        fun `konstruktør i ValgteFelt skal kaste feil hvis valgte verdier er tomme`() {
+            val thrown = shouldThrow<IllegalArgumentException> {
+                OpplaringKategoriseringValg.ValgteFelt(
+                    representerer = OpplaringKategoriseringType.FORERKORT,
+                    valg = emptyMap(),
+                )
+            }
+
+            thrown.message shouldBe "ValgteFelt må inneholde minst ett valg"
+        }
+
+        @Test
+        fun `konstruktør i ValgteFelt skal ikke kaste feil ved gyldige verdier`() {
+            val valgteFelt = OpplaringKategoriseringValg.ValgteFelt(
+                representerer = OpplaringKategoriseringType.FORERKORT,
+                valg = mapOf(UUID.randomUUID() to "B", UUID.randomUUID() to "C"),
+            )
+
+            valgteFelt.representerer shouldBe OpplaringKategoriseringType.FORERKORT
+            valgteFelt.valg.size shouldBe 2
+        }
+    }
+
+    @Nested
     inner class HentVerdierTests {
         @Test
         fun `hentVerdier skal returnere verdier for gitt kategoriseringstype`() {
