87a6d0667f50b311a7488d653f18aebf2f5e515c
Roar Skinderviken <7499642+roar-skinderviken@users.noreply.github.com>
2026-08-28 11:22:05 +0200

    Endrer eier av app-tabeller til app-bruker (#436)

--- amt-aktivitetskort-publisher/src/main/resources/db/migration/V26__change-table-owner-to-app-user.sql
+++ amt-aktivitetskort-publisher/src/main/resources/db/migration/V26__change-table-owner-to-app-user.sql
@@ -1,47 +1,6 @@
-DO
-$$
-    DECLARE
-        app_user name;
-        table_record RECORD;
-    BEGIN
-        -- Nothing to do if no public tables are owned by cloudsqlsuperuser
-        IF NOT EXISTS (
-            SELECT 1
-            FROM pg_tables
-            WHERE schemaname = 'public'
-              AND tableowner = 'cloudsqlsuperuser'
-        ) THEN
-            RAISE NOTICE 'No public tables owned by cloudsqlsuperuser. Nothing to do.';
-            RETURN;
-        END IF;
-
-        -- Find the application user
-        SELECT rolname
-        INTO STRICT app_user
-        FROM pg_roles
-        WHERE rolname LIKE 'amt-aktivitetskort-publisher%'
-          AND rolcanlogin;
-
-        RAISE NOTICE 'Changing table owners to %', app_user;
-
-        -- Change owner of all public tables owned by cloudsqlsuperuser
-        FOR table_record IN
-            SELECT schemaname, tablename
-            FROM pg_tables
-            WHERE schemaname = 'public'
-              AND tableowner = 'cloudsqlsuperuser'
-            LOOP
-                RAISE NOTICE 'Changing owner of %.% to %',
-                    table_record.schemaname,
-                    table_record.tablename,
-                    app_user;
-
-                EXECUTE format(
-                        'ALTER TABLE %I.%I OWNER TO %I',
-                        table_record.schemaname,
-                        table_record.tablename,
-                        app_user
-                        );
-            END LOOP;
-    END
-$$;
+ALTER TABLE arrangor OWNER TO CURRENT_USER;
+ALTER TABLE deltaker OWNER TO CURRENT_USER;
+ALTER TABLE deltakerliste OWNER TO CURRENT_USER;
+ALTER TABLE feilmelding OWNER TO CURRENT_USER;
+ALTER TABLE melding OWNER TO CURRENT_USER;
+ALTER TABLE oppfolgingsperiode OWNER TO CURRENT_USER;
