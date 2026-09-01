d173dfed5364f4ed9303babb5d7d6736d655fe80
Roar Skinderviken <roar.skinderviken@gmail.com>
2026-09-01 15:39:29 +0200

    Bruker nå injected objectMapper

--- amt-tiltaksarrangor-bff/src/main/kotlin/no/nav/tiltaksarrangor/consumer/KafkaConsumer.kt
+++ amt-tiltaksarrangor-bff/src/main/kotlin/no/nav/tiltaksarrangor/consumer/KafkaConsumer.kt
@@ -4,11 +4,11 @@ import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskoder.skalKometLag
 import no.nav.tiltaksarrangor.consumer.model.TiltakstypePayload
 import no.nav.tiltaksarrangor.melding.MELDING_TOPIC
 import no.nav.tiltaksarrangor.repositories.TiltakstypeRepository
-import no.nav.tiltaksarrangor.utils.objectMapper
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
+import tools.jackson.databind.ObjectMapper
 import tools.jackson.module.kotlin.readValue
 import java.util.UUID
 
@@ -17,6 +17,7 @@ class KafkaConsumer(
     private val kafkaConsumerService: KafkaConsumerService,
     private val deltakerlisteConsumerService: DeltakerlisteConsumerService,
     private val tiltakstypeRepository: TiltakstypeRepository,
+    private val objectMapper: ObjectMapper,
 ) {
     @KafkaListener(
         topics = [
