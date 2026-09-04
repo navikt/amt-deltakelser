d23349075c196f5b80180060ad7845f40e6632f7
Marthe Frogner Holm <marthe.frogner.holm@nav.no>
2026-09-04 13:29:07 +0200

    fiks skrivefeil

--- amt-pdfgen/src/test/kotlin/no/nav/amt/pdfgen/EndringsvedtakTest.kt
+++ amt-pdfgen/src/test/kotlin/no/nav/amt/pdfgen/EndringsvedtakTest.kt
@@ -191,6 +191,7 @@ class EndringsvedtakTest :
 
                         doc.text() shouldContain "Fritekst om hva opplæringen inneholder"
                         doc.text() shouldContain "Bransje: Bygg og anlegg"
+                        doc.text() shouldNotContain "Dette er ledeteksten"
                     }
 
                     it("viser standard innholdsliste når flagget ikke er med i data") {
