7bd94c264fc092071b2f23d50c68f0345fd227f5
Roar Skinderviken <roar.skinderviken@gmail.com>
2026-09-08 13:35:12 +0200

    komet-bau: Fixes for CVE-2026-59949 og CVE-2026-65182. Bumper Kafka Manager til 2026.09.08-08.59-356e4e6

--- amt-tiltaksarrangor-bff/build.gradle.kts
+++ amt-tiltaksarrangor-bff/build.gradle.kts
@@ -17,7 +17,19 @@ repositories {
     maven { setUrl("https://github-package-registry-mirror.gc.nav.no/cached/maven-release") }
 }
 
+// midlertidig fix for CVE-2026-65182
+extra["tomcat.version"] = "11.0.25"
+
 dependencies {
+    constraints {
+        implementation("at.yawk.lz4:lz4-java") {
+            version {
+                strictly("1.11.2")
+            }
+            because("Fixes CVE-2026-59949")
+        }
+    }
+
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
