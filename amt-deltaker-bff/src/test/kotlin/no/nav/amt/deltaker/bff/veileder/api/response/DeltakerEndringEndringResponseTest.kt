1bfb4e8757feab680841c439c2cc8da845232f7f
Jarle Fagerheim <jarle.fagerheim@nav.no>
2026-09-15 15:06:33 +0200

    ekstra test

--- amt-deltaker-bff/src/test/kotlin/no/nav/amt/deltaker/bff/veileder/api/response/DeltakerEndringEndringResponseTest.kt
+++ amt-deltaker-bff/src/test/kotlin/no/nav/amt/deltaker/bff/veileder/api/response/DeltakerEndringEndringResponseTest.kt
@@ -1,5 +1,6 @@
 package no.nav.amt.deltaker.bff.veileder.api.response
 
+import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeInstanceOf
 import no.nav.amt.deltaker.bff.commonresponse.PrisinformasjonResponse
 import no.nav.amt.lib.models.deltaker.DeltakerEndring
@@ -70,5 +71,22 @@ class DeltakerEndringEndringResponseTest {
         response
             .prisinfo
             .shouldBeInstanceOf<PrisinformasjonResponse.IngenKostnader>()
+    }
+
+    @Test
+    fun `endrePrisinfo - tilbakekalt status - mappes korrekt`() {
+        // Arrange
+        val endring = DeltakerEndring.Endring.EndrePrisinfo(
+            prisinfo = PrisinformasjonDto.Anskaffelse(pris = 5000),
+            begrunnelse = "Begrunnelse",
+            status = DeltakerEndring.Endring.EndrePrisinfo.Status.TILBAKEKALT,
+        )
+
+        // Act
+        val response = DeltakerEndringEndringResponse.fromModel(endring, null)
+            .shouldBeInstanceOf<DeltakerEndringEndringResponse.EndrePrisinfo>()
+
+        // Assert
+        response.status shouldBe DeltakerEndring.Endring.EndrePrisinfo.Status.TILBAKEKALT
     }
 }
