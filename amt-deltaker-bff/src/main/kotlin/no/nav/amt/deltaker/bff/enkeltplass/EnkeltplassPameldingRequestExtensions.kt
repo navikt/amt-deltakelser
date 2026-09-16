2ee7721fb5e00c054b5c1f0c9d21a7fef9ba775d
Roar Skinderviken <7499642+roar-skinderviken@users.noreply.github.com>
2026-09-16 10:16:26 +0200

    komet-bau: Tillater nå organisasjonsnummer som inneholder 9 siffer uten at de starter med 8 eller 9 (#480)

--- amt-deltaker-bff/src/main/kotlin/no/nav/amt/deltaker/bff/enkeltplass/EnkeltplassPameldingRequestExtensions.kt
+++ amt-deltaker-bff/src/main/kotlin/no/nav/amt/deltaker/bff/enkeltplass/EnkeltplassPameldingRequestExtensions.kt
@@ -3,14 +3,16 @@ package no.nav.amt.deltaker.bff.enkeltplass
 import io.ktor.server.plugins.requestvalidation.ValidationResult
 import no.nav.amt.internapi.enkeltplass.EnkeltplassPameldingRequest
 
-val ORGNUMMER_REGEX = Regex("^[89]\\d{8}$")
+// Norske orgnr starter med '8' eller '9'. Utenlandske og test-orgnr følger ikke dette mønsteret.
+// Krever derfor kun at orgnr inneholder 9 siffer.
+val ORGNUMMER_REGEX = Regex("^\\d{9}$")
 
 fun EnkeltplassPameldingRequest.validate(): ValidationResult = when {
     beskrivelse.isBlank() -> ValidationResult.Invalid("Beskrivelse kan ikke være tom")
     arrangorUnderenhet.isBlank() -> ValidationResult.Invalid("Arrangør orgnummer kan ikke være tom")
     !ORGNUMMER_REGEX.matches(
         arrangorUnderenhet,
-    ) -> ValidationResult.Invalid("Organisasjonsnummeret må starte med 8 eller 9 og inneholde 9 siffer")
+    ) -> ValidationResult.Invalid("Organisasjonsnummeret må inneholde 9 siffer")
 
     else -> {
         prisinformasjon
