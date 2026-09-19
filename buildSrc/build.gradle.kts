6aea34d0ee9a323145dc36750aa75f0c30ade01a
Roar Skinderviken <7499642+roar-skinderviken@users.noreply.github.com>
2026-09-19 15:15:05 +0200

    komet-bau: Lagt til convention plugin for Spring Boot (#493)

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,4 +12,7 @@ dependencies {
     implementation(libs.kotlin.jvm)
     implementation(libs.ktor.plugin)
     implementation(libs.ktlint.gradle.plugin)
+    implementation(libs.spring.boot.gradle.plugin)
+    implementation(libs.spring.dependency.management.gradle.plugin)
+    implementation(libs.kotlin.spring.gradle.plugin)
 }
