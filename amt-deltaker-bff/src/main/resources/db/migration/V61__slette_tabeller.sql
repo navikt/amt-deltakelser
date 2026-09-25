4c0fe16a5c573e837ab2b25ad5b1f4d8ff4c9d23
Izis Frøyhov <izis.froyhov@nav.no>
2026-09-25 08:43:20 +0200

    Legger inn sjekk på om tabellene finnes før de slettes i tilfelle scriptet skulle krasje ved kjøring

--- amt-deltaker-bff/src/main/resources/db/migration/V61__slette_tabeller.sql
+++ amt-deltaker-bff/src/main/resources/db/migration/V61__slette_tabeller.sql
@@ -1,7 +1,7 @@
-drop table forslag;
-drop table vurdering;
-drop table ulest_hendelse;
-drop table deltaker_status;
-drop table deltaker;
-drop table nav_bruker;
-drop table nav_enhet;
+drop table if exists forslag;
+drop table if exists vurdering;
+drop table if exists ulest_hendelse;
+drop table if exists deltaker_status;
+drop table if exists deltaker;
+drop table if exists nav_bruker;
+drop table if exists nav_enhet;
